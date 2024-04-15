255eb41715e4d505d7be58aeb523eaa54d1dfbe1
n126 <757906626@qq.com>
2024-04-15 12:05:53 +0000

    test debug

--- src/dare_ep_db.c
+++ src/dare_ep_db.c
@@ -170,6 +170,7 @@ free_ep(dare_ep_t *ep)
     ud_ah_destroy(ep->ud_ep.ah);
 
     /*释放write time*/
+    info(log_fp, "free ep\n");
     write_time_t *current_write, *tmp;
     HASH_ITER(hh, ep->write_time, current_write, tmp) {
          HASH_DEL(ep->write_time, current_write);
