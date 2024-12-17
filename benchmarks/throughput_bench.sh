7aed16512be20fc2997fffa250d1175ca3de3e70
dwyaneli <757906626@qq.com>
2024-12-17 11:22:45 +0800

    test

--- benchmarks/throughput_bench.sh
+++ benchmarks/throughput_bench.sh
@@ -106,7 +106,7 @@ CreatrTraceForClients() {
     echo "create trace for clients"
     for i in "${clients[@]}"; do
         create_trace=( "${DAREDIR}/bin/kvs_trace" "--loop" "--${OPCODE}" "-s ${blob_size}" "-o $trace_file" )
-        cmd=("ssh" "$USER@$i" "rm -f $trace_file")
+        cmd=("ssh" "$USER@$i" "rm -rf $trace_file")
         echo "Executing: ${cmd[@]}"
         ${cmd[@]}
         cmd=("ssh" "$USER@$i" "${create_trace[@]}")
@@ -119,7 +119,7 @@ echo "start!"
 DAREDIR=""
 OPCODE="put"
 server_count=3
-client_count=8
+client_count=4
 blob_size=64
 proc=100
 for arg in "$@"
@@ -193,9 +193,8 @@ echo ">>> ${server_count} servers: ${servers[@]}"
 if [ "x$OPCODE" != "xput" -a "x$OPCODE" != "xget" ]; then
     ErrorAndExit "Wrong operation type: --op."
 fi
-rm -rf data
 rm -f *.log
-mkdir -p data
+# mkdir -p data
 trace_file="$PWD/data/loop_req_${OPCODE}_${blob_size}b.trace"
 
 # Handle SIGINT to ensure a clean exit
@@ -215,7 +214,7 @@ echo -ne "Starting $server_count servers...\n"
 StartDare $server_count
 echo "done"
 
-sleep 2
+sleep 10
 
 # Write entry in the SM
 tmp_tfile="$PWD/tmp.trace"
@@ -231,10 +230,9 @@ CreatrTraceForClients
 StartClients
 sleep 10
 StopClients
+pkill clt_test
 
 sleep 0.2
 StopDare
 
 ########################################################################
-
-
